4058c7abc52dd7ad55b846add1621a1229d81037
letuanhai <letuanhai@live.com>
2024-04-05 00:27:50 +0200

    feat: add templates for index

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
-from typing import Annotated
-
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get('/', response_class=HTMLResponse)
-@app.get('/index', response_class=HTMLResponse)
-def index():
-    return 'Hello, World!'
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+@app.get("/index", response_class=HTMLResponse)
+def index(
+    request: Request,
+):
+    user = {"username": "hai"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    return templates.TemplateResponse(
+        request=request,
+        name="index.jinja-html",
+        context=dict(title="Home", user=user, posts=posts),
+    )
